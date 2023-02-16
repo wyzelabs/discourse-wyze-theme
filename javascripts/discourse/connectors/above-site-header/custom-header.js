55a0481e495155134d07b7add713a2ba16b96f8f
awesomerobot <kris.aubuchon@discourse.org>
2023-02-16 10:53:19 -0500

    FIX: custom header forum dropdown

--- javascripts/discourse/connectors/above-site-header/custom-header.js
+++ javascripts/discourse/connectors/above-site-header/custom-header.js
@@ -3,22 +3,40 @@ export default {
     component.setProperties({
       didInsertElement() {
         this._super(...arguments)
-        ;(function ($) {
-          $('.dropdown-toggle').hover(
-            function () {
-              $(this)
-                .children('.community-dropdown')
-                .stop(true, true)
-                .slideDown('fast')
-            },
-            function () {
-              $(this)
-                .children('.community-dropdown')
-                .stop(true, true)
-                .slideUp('slow')
-            }
-          )
-        })(jQuery)
+
+        if (!this.site.mobileView) {
+          ;(function ($) {
+            $('.dropdown-toggle').hover(
+              function () {
+                $(this)
+                  .children('.community-dropdown')
+                  .stop(true, true)
+                  .slideDown('fast')
+              },
+              function () {
+                $(this)
+                  .children('.community-dropdown')
+                  .stop(true, true)
+                  .slideUp('slow')
+              }
+            )
+          })(jQuery)
+        }
+
+        if (this.site.mobileView) {
+          $(function () {
+            $('.dropdown-toggle').on('click', function () {
+              event.stopPropagation()
+              $('.community-dropdown').toggle()
+            })
+            $('.community-dropdown').on('click', function (event) {
+              event.stopPropagation()
+            })
+            $(document).on('click', function () {
+              $('.community-dropdown').hide()
+            })
+          })
+        }
       },
     })
   },
